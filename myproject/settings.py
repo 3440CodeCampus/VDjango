ce3f557eda42ab34d82ad7843dc889f6f7b70ddb
3440CodeCampus <mtlgdroid@gmail.com>
2018-07-02 21:27:15 -0400

    added gmail connection

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,3 +132,13 @@ LOGIN_REDIRECT_URL='home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+'''
+# Gmail
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='mtlgdroid@gmail.com'
+EMAIL_HOST_PASSWORD='mpec3440#'
+EMAIL_USE_TLS=True
+# end gmail
+'''
