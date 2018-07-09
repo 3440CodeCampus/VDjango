f0d8b9ac3371fa06f7f276bca34c524e50afb28d
3440CodeCampus <mtlgdroid@gmail.com>
2018-07-09 15:28:26 -0400

    reply is done

--- boards/views.ver0.py
+++ boards/views.ver0.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from .forms import NewTopicForm, PostForm
+from .forms import NewTopicForm
 from .models import Board,  Post, Topic
 from django.contrib.auth.decorators import login_required
 
@@ -15,39 +15,21 @@ def board_topics(request,pk):
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # new  after forms.py
+
     if request.method == "POST":
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = request.user
+            topic.starter = user
             topic.save()
             post = Post.objects.create(
                 message = form.cleaned_data.get('message'),
                 topic = topic,
-                created_by = request.user
+                created_by = user
             )
-            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
+            return redirect('board_topics', pk=board.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board':board , 'form':form})
-
-def topic_posts(request,pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    return render(request, 'topic_posts.html',{'topic':topic})
-
-@login_required
-def reply_topic(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.topic = topic
-            post.created_by = request.user
-            post.save()
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
-    else:
-        form = PostForm()
-    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
-
